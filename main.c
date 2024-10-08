66208b9863e4fb2de976ecd1666849fe0f185028
unknown <vuongngocthien222@gmail.com>
2024-10-09 00:03:26 +0700

    update main

--- main.c
+++ main.c
@@ -1,50 +1,65 @@
-#include "main.h"                  
+#include "main.h"    
+uint32_t IC1Val = 0;
+uint32_t IC2Val = 0;
+float Freq = 0;
+float Duty = 0;
+uint32_t msTime = 0;
+void TIM2_PWMIC();
+void TIM1_PWMOC(uint32_t Freq, uint32_t Duty_Cycle);
+void SystemInit()
+{
+}
 void TIM2_IRQHandler()
 {
-    
+    if(TIM2->SR & TIM_SR_CC1IF)
+		{
+			IC1Val = TIM2->CCR1;
+			TIM2->SR &= ~TIM_SR_CC1IF;
+		}
+		if(TIM2->SR & TIM_SR_CC2IF)
+		{
+			IC2Val = TIM2->CCR2;
+			TIM2->SR &= ~TIM_SR_CC2IF;
+		}
+		Duty = IC2Val*100 / IC1Val;
+		Freq = RCC_GetAbp1Clk()/IC1Val;
 }
 
 int main()
-{
-	// Enable clock for GPIOA
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	// Enable clock for TIM2
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	
-	// Configure Channel 1 in PWM input mode
-	TIM2->CCMR1 |= (1 << 0);              // CC1S = 01, CC1 channel is configured as input, IC1 is mapped on TI1
-	TIM2->CCER &= ~TIM_CCER_CC1P;         // Capture is done on a rising edge of IC1
-	
-	// Configure Channel 2 in PWM input mode
-	TIM2->CCMR2 |= (1 << 8);              // CC2S = 10, CC2 channel is configured as input, IC2 is mapped on TI1
-	TIM2->CCER |= TIM_CCER_CC2P;          // Capture is done on a falling edge of IC1
-	
-	// Trigger selection: Filtered Timer Input 1
-	TIM2->SMCR |= (5 << 4);
-	
-	// Slave mode selection: Reset Mode
-	TIM2->SMCR |= (1 << 3);
-	
-	// Enable capture for Channel 1
-	TIM2->CCER |= TIM_CCER_CC1E;  
-	
-	// Enable capture for Channel 2
-	TIM2->CCER |= TIM_CCER_CC2E;  
-	
-	// Enable TIM2
-	TIM2->CR1 |= TIM_CR1_CEN;
-	
-	TIM2->CCER  |= TIM_CCER_CC1E;  //CH1 Capture enable
-	TIM2->CCER  |= TIM_CCER_CC2E;  //CH2 Capture enable 
-	TIM2->CR1 |= TIM_CR1_CEN;
-	
-	
-//	RCC->APB2ENR = RCC_APB2ENR_IOPCEN;
-//	TIM2->DIER |= TIM_DIER_CC1IE;
-//  NVIC_EnableIRQ(TIM2_IRQn);
+{	
+	TIM1_PWMOC(10000, 30);
+	TIM2_PWMIC();
 	while(1)
 	{
 		
 	}
 	return 0;
+}
+
+void TIM1_PinConfig()
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	GPIO_SetMode(GPIOA, 8, GPIO_ALT_MODE_50M_PP);	
+}
+
+void TIM1_PWMOC(uint32_t Frequency, uint32_t Duty_Cycle)
+{
+	TIM1_PinConfig();
+	TIM_OC1_PWMM1_Init(TIM1, Frequency, Duty_Cycle);
+}
+
+void TIM2_PinConfig()
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	GPIO_SetMode(GPIOA, 0, GPIO_INPUT_MODE_FLOAT);
+}
+
+void TIM2_PWMIC()
+{
+	TIM2_PinConfig();
+	TIM_PWMICMInit(TIM2);
+	
+	//Enable Global Interrupt
+	NVIC_SetPriority(TIM2_IRQn, 1);
+	NVIC_EnableIRQ(TIM2_IRQn);
 }
--- main.h
+++ main.h
@@ -1,2 +1,9 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
 #include "GPIO.h"
 #include "RCC.h"
+#include "USART.h"
+#include "TIM.h"
+
+#endif /* __MAIN_H__ */
