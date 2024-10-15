2322a33997a67df7526dc34a604567f118c51465
unknown <vuongngocthien222@gmail.com>
2024-10-15 08:19:47 +0700

    UART driver

---
+++ Code/USART.c
@@ -0,0 +1,46 @@
+#include "USART.h"
+void USART_Init(USART_TypeDef* USARTx, uint32_t BAUD_RATE){
+    uint32_t fclk = 0;
+    //Enable AFIO RCC
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+    //Set pin to USART Mode
+    if(USARTx == USART1)
+    {
+        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+        fclk = RCC_GetAbp2Clk();
+    }
+    else if(USARTx == USART2)
+    {
+        RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+        fclk = RCC_GetAbp1Clk();
+    }
+    else if(USARTx == USART3)
+    {
+        RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
+        fclk = RCC_GetAbp1Clk();
+    }
+    //Calculate the BAUDRATE
+	  double USART_DIV = 1.0 * fclk / (BAUD_RATE << 4) ;
+    USARTx->BRR |= (uint32_t)USART_DIV << 4;
+    USARTx->BRR |= (uint32_t)((USART_DIV - (uint32_t)USART_DIV) * 16);
+}
+
+void USART_send(USART_TypeDef *USARTx, unsigned char c)
+{
+				while(!(USARTx->SR & USART_SR_TXE)){;}
+        USARTx->DR = c;
+}
+
+void USART_str(USART_TypeDef *USARTx, unsigned char *str)
+{
+	for(int i = 0; str[i] != '\0'; i++)
+	{
+		USART_send(USARTx, str[i]);
+	}
+}
+
+unsigned char USART_receive(USART_TypeDef *USARTx)
+{
+    while(!(USARTx->SR & USART_SR_RXNE)){;}
+    return (char)USARTx->DR;
+}
---
+++ Code/USART.h
@@ -0,0 +1,12 @@
+#ifndef __USART_H__
+#define __USART_H__
+
+#include "stm32f10x.h"                  // Device header
+#include "RCC.h"
+
+void USART_Init(USART_TypeDef* USARTx, uint32_t BAUD_RATE);
+void USART_send(USART_TypeDef *USARTx, unsigned char c);
+void USART_str(USART_TypeDef *USARTx, unsigned char *str);
+unsigned char USART_receive(USART_TypeDef *USARTx);
+
+#endif /* __USART_H__ */
