8f822a6f53f7e91b6d9ab8ec16fa2b6fa0bec700
unknown <vuongngocthien222@gmail.com>
2024-10-22 22:06:15 +0700

    UART Init, sending data

--- Code/main.c
+++ Code/main.c
@@ -1,73 +1,89 @@
-#include "main.h"    
-uint32_t IC1Val = 0;
-uint32_t IC2Val = 0;
-float Freq = 0;
-float Duty = 0;
-uint32_t msTime = 0;
-char signal_present = 0;
-void TIM2_PWMIC();
-void TIM1_PWMOC(uint32_t Freq, uint32_t Duty_Cycle);
-//void SystemInit()
-//{
-//}
-void TIM2_IRQHandler()
-{
-    if (TIM2->SR & TIM_SR_CC1IF)
-    {
-        TIM2->SR &= ~TIM_SR_CC1IF;  
-        IC1Val = TIM2->CCR1;  				
-    }
-    if (TIM2->SR & TIM_SR_CC2IF)
-    {
-        TIM2->SR &= ~TIM_SR_CC2IF;  
-        IC2Val = TIM2->CCR2;  
-    }
-    Duty = (float)IC2Val * 100 / IC1Val;  
-    Freq = (float)RCC_GetAbp1Clk() / IC1Val;  
+#include "main.h"
 
-    if (TIM2->SR & TIM_SR_UIF)
-    {
-				TIM2->CNT = 0;
-        TIM2->SR &= ~TIM_SR_UIF;  
+// Interrupt Service Routine for Timer 2
+void TIM2_IRQHandler() {
+    if (TIM2->SR & TIM_SR_CC1IF) { // Check for capture/compare 1 interrupt flag
+        TIM2->SR &= ~TIM_SR_CC1IF; // Clear the interrupt flag
+        IC1Val = TIM2->CCR1; // Read the captured value
+        if (stringSent) { // If the frequency and duty cycle string has been sent
+            USART_send(USART1, '1'); // Send '1' via UART
+        }
+    } else if (TIM2->SR & TIM_SR_CC2IF) { // Check for capture/compare 2 interrupt flag
+        TIM2->SR &= ~TIM_SR_CC2IF; // Clear the interrupt flag
+        IC2Val = TIM2->CCR2; // Read the captured value
+        if (stringSent) { // If the frequency and duty cycle string has been sent
+            USART_send(USART1, '0'); // Send '0' via UART
+        }
+    }
+
+    // Calculate duty cycle and frequency
+    Duty = IC2Val * 100 / IC1Val;
+    Freq = (uint32_t)(RCC_GetAbp1Clk() / (IC1Val * (TIM2->PSC + 1)));
+
+    // Updatefrequency or duty cycle if change 
+    if (old_freq != Freq || old_duty != (uint32_t)Duty) {
+        update = 1; // Set update flag
+        stringSent = 0; // Reset stringSent flag
     }
 }
 
-int main()
-{	
-	TIM1_PWMOC(10000, 50);
-	TIM2_PWMIC();
-	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-	GPIO_SetMode(GPIOB, 11, GPIO_OUTPUT_MODE_50M_PP);
-	
-	while(1)
-	{
-	 
-	}
-	return 0;
+int main() {
+    // Configure GPIO and timers
+    TIM1_PWMOC(20000, 30);
+    TIM2_PWMIC();
+
+    char buffer[50];
+    int check = 0;
+
+    while (1) {
+        if (update == 1) { // If update flag is set
+            old_freq = Freq; // Update old frequency
+            old_duty = Duty; // Update old duty cycle
+            update = 0; // Reset update flag
+            sprintf(buffer, "F:%uD:%u\n", Freq, Duty); // Format frequency and duty cycle string
+            USART_str(USART1, (unsigned char *)buffer); // Send string via UART
+            check++;
+        }
+        if (check >= 2) { // After sending the string twice
+            check = 0; // Reset check counter
+            stringSent = 1; // Set stringSent flag
+        }
+    }
+    return 0;
 }
 
-void TIM1_PinConfig()
+//Configure USART1
+void USART1_Config(uint32_t BAUD_RATE)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	GPIO_SetMode(GPIOA, 8, GPIO_ALT_MODE_50M_PP);	
-	GPIO_SetMode(GPIOA, 9, GPIO_ALT_MODE_50M_PP);	
+	// Initialize UART1
+  USART_Init(USART1, BAUD_RATE);
+	// Configure GPIO for UART
+  GPIO_SetMode(GPIOA, 9, GPIO_ALT_MODE_50M_PP);
+  GPIO_SetMode(GPIOA, 10, GPIO_INPUT_MODE_FLOAT);
+	// UART1 Enable
+  USART1->CR1 |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
 }
 
-void TIM1_PWMOC(uint32_t Frequency, uint32_t Duty_Cycle)
-{
-	TIM1_PinConfig();
-	TIM_OC1_PWMM1_Init(TIM1, Frequency, Duty_Cycle);
+// Configure GPIO for Timer 1 and UART
+void TIM1_PinConfig() {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    GPIO_SetMode(GPIOA, 6, GPIO_ALT_MODE_50M_PP);
 }
 
-void TIM2_PinConfig()
-{
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	GPIOA->ODR |= GPIO_ODR_ODR0;
-	GPIO_SetMode(GPIOA, 0, GPIO_INPUT_MODE_PuPd);
+// Initialize PWM output for Timer 1 function
+void TIM1_PWMOC(uint32_t Frequency, uint32_t Duty_Cycle) {
+    TIM1_PinConfig();
+    TIM_OC1_PWMM1_Init(TIM1, Frequency, Duty_Cycle);
 }
 
-void TIM2_PWMIC()
-{
+// Configure GPIO for Timer 2
+void TIM2_PinConfig() {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    GPIO_SetMode(GPIOA, 0, GPIO_INPUT_MODE_FLOAT);
+}
+
+// Initialize PWM input capture for Timer 2 function
+void TIM2_PWMIC() {
     TIM2_PinConfig();
     TIM_PWMICMInit(TIM2);
     NVIC_SetPriority(TIM2_IRQn, 1);
--- Code/main.h
+++ Code/main.h
@@ -5,5 +5,22 @@
 #include "RCC.h"
 #include "USART.h"
 #include "TIM.h"
+#include <stdio.h>
 
+// Declare global variables and flags
+volatile uint32_t IC1Val = 0; // CCR value when catching rising edge
+volatile uint32_t IC2Val = 0; // CCR value when catching falling edge
+volatile uint32_t Freq = 0;		// Input frequency
+volatile int Duty = 0;				// Input Duty Cycle
+uint32_t old_freq = 0;
+uint32_t old_duty = 0;
+volatile int stringSent = 0; // Flag to check if the frequency and duty cycle string has been sent
+volatile int update = 0; 		 // Flag to indicate if the frequency and duty cycle need to be updated
+
+//Configure USART1
+void USART1_Config(uint32_t BAUD_RATE);
+// Initialize PWM output for Timer 1 function
+void TIM1_PWMOC(uint32_t Frequency, uint32_t Duty_Cycle);
+// Initialize PWM input capture for Timer 2 function
+void TIM2_PWMIC();
 #endif /* __MAIN_H__ */
